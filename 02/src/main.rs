0a54c5af73bf541ca77b9db304cfa2084b75af6f
ggarver <gmgarver@willamette.edu>
2026-01-22 20:00:39 -0800

    print functions for char flags

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -1,4 +1,3 @@
-use std::string::String;
 use std::env;
 use std::fs;
 
@@ -7,65 +6,82 @@ fn main() {
     let mut bytes = false;
     let mut chars = false;
     let mut lines = false;
-    let mut max_lines = false;
+    let mut max_l = false;
     let mut words = false;
-
-
-    // get file path from cl arg
+    let mut file_f = false;
     let mut files = Vec::new();
-    let path: String = env::args().nth(1).unwrap();
 
     // check if character flags - otherwise try to read file
     // try to iter through args
     for arg in env::args().skip(1) {
         if arg.starts_with('-') {
             if arg.contains("c"){ 
-                let bytes = true;
+                bytes = true;
             } else if arg.contains("m"){
-                let chars = true;
+                chars = true;
             } else if arg.contains("l"){
-                let lines = true;
+                lines = true;
             } else if arg.contains("L"){
-                let max_lines = true;
+                max_l = true;
             } else if arg.contains("w"){
-                let words = true;
+                words = true;
             }
             // string flags
             if arg.starts_with("--") {
                 if arg == "--bytes"{
-                    let bytes = true;
+                    bytes = true;
                 } else if arg == "--chars"{
-                    let chars = true;
+                    chars = true;
                 } else if arg == "--lines"{
-                    let lines = true;
+                    lines = true;
                     // This one needs to be fixed
                 } else if arg == "--files0-from=F"{
-                    let file_f = true;
+                    file_f = true;
                 } else if arg == "--max-line-length"{
-                    let max_l = true;
+                    max_l = true;
                 } else if arg == "--words"{
-                    let words = true;
+                    words = true;
                 }
             }
             // if not flag, should be file
         } else {
             files.push(arg);
         }
+    }
 
-        for filepath in files {
-            let cont = fs::read_to_string(&filepath).unwrap();
+    for filepath in &files {
+        let cont = fs::read_to_string(&filepath).unwrap();
 
-            // calculate counts
-            let b_counts = cont.bytes().count();
-            let l_count = cont.lines().count();
-            let w_count = cont.words().count();
-            let c_count = cont.chars().count();
-            //let ml_length = cont...
-        }        
+        // calculate counts
+        let b_count = cont.bytes().count();
+        let l_count = cont.lines().count();
+        let c_count = cont.chars().count();
+        let w_count = cont.split_whitespace().count();
 
-        //base case no flags
-        if !bytes && !chars && !lines && !max_lines && !words {
-            println!(" {} {} {} {}", l_count, w_count, b_count, path);
+        // let ml_length = cont...
+
+
+        // print for char flags
+        if bytes || chars || lines || max_l || words {
+            if lines {
+                println!("{}", l_count);
+            }
+            if words {
+                println!("{}", w_count);
+            }
+            if chars {
+                println!("{}", c_count);
+            }
+            if bytes {
+                println!("{}", b_count);
+            }
+            if max_l {
+                println!("NA");
+            }
+            println!("{}", filepath);
+        } else {
+            println!(" {} {} {} {}", l_count, w_count, b_count, filepath);
+
         }
     }
 }
