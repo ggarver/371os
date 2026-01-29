0bedc73bd452b4aea1910a84c8432ebb82bb5091
ggarver <gmgarver@willamette.edu>
2026-01-28 19:03:06 -0800

    add gitignore

--- 21/target/debug/deps/hello_world-b3541396868c5619.d
+++
@@ -1,5 +0,0 @@
-/home/grace/371os/21/target/debug/deps/hello_world-b3541396868c5619.d: src/main.rs
-
-/home/grace/371os/21/target/debug/deps/hello_world-b3541396868c5619: src/main.rs
-
-src/main.rs:
--- 21/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/home/grace/371os/21/target/debug/hello_world: /home/grace/371os/21/src/main.rs
