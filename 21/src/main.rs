a21e49db2976419281de1bf53e3ad578ccbe4425
ggarver <gmgarver@willamette.edu>
2026-01-28 21:26:39 -0800

    finish up lab + simplify

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -1,18 +1,12 @@
-// numeric addresses
 fn main() {
-    // hello world in f32
-    let address = [1.1431391e27, 6.6578487e28, 7.739293e-19, 1.4e-44, 9.5946e-41, 1.04704e-40, 9.5982e-41, 1.05005e-40, 1.05219e-40, 9.5946e-41, 9.5946e-41, 9.5946e-41];
-    let mut address_u32: Vec<u32> = Vec::new();
-    for x in &address {
-        unsafe { 
-            let words_f32: &u32 = std::mem::transmute::<&f32, &u32>(x);
-            address_u32.push(*words_f32);
-        }
-    }
     unsafe {
-        // let fin_words = 
-
+        // transmute changes type without altering underlying memory
+        
+        // Hello World! in i32s, i32=4bytes
+        let address: [i32; 3] = [1819043144, 1867980911, 560229490];
+        // each byte = char, "Hello World!" = 12
+        let chars: &[u8; 12] = std::mem::transmute(&address);
+        println!("{:?}", std::str::from_utf8_unchecked(chars));
     }
-    println!("{:?}", address_u32);
 }
 
