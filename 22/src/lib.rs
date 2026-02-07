031d95bff227ecf88322d6b92e2301e30c26bb5f
ggarver <gmgarver@willamette.edu>
2026-02-06 16:24:31 -0800

    malloc hw

--- 22/src/lib.rs
+++ 22/src/lib.rs
@@ -1,43 +1,100 @@
 // Return an index in BUS of s reserved bytes
 
-// Zero the array except the mask.
+// size is amnt of bytes 
+pub const SIZE:usize = 0x80;
+static mut BUS: [u8; SIZE] = [0u8;SIZE];
+const res: usize = SIZE >> 3 >> 3;
+
+
 // reserve the beginning of the array for bitmask
 fn init() {
     unsafe {
-        // Initialize mask
+        // Initialize mask - zero array
         // The following explodes if SIZE isn't a power of 2
         assert!(SIZE & (SIZE - 1) == 0);
         // First SIZE >> 3 bits are reserved as a validty byte/bit mask
-        < snip >
-        // Which has to reserve enough bytes for itself.
-        < snip >
-        // Set to 1
-        < snip >
 
-        // Initialize memory
-        // Set to zero.
-        < snip >
+        // if a bit is set to 1, corresponding byte is in use 
+        // Which has to reserve enough bytes for itself.
+        // Going from 0 to max reserved bytes
+        for i in 0..res {
+            BUS[i] = 0b11111111;
+        }         
     }
     return;
 }
 
+fn search_space(s: usize, bindex: usize, zcount: usize) -> Option<usize> {
+    let bydex = bindex / 8; // byte index 
+    let local_bindex = bindex % 8;
+    if bydex >= SIZE { // not enough space 
+        return None;
+    } 
+    if (unsafe {BUS[bydex]} >> local_bindex) & 1 == 0 {
+        if zcount + 1 == s { // if enough space
+            return Some(bindex);
+        } else {
+            return search_space(s, bindex+1, zcount+1);
+        } 
+    } else { // found a 1 
+        return search_space(s, bindex+1, 0);
+    }
 
 
+}
 
+fn update_bmask(start: usize, end:usize) {
+    if start == end {
+        return;
+    } else {
+        let bydex = start / 8;
+        let local_bindex = start % 8;
+        unsafe {BUS[bydex] |= (1 << local_bindex);}
+        update_bmask(start+1, end);
+    }
+}
+
+// usize = # of bytes to allocate 
 pub fn malloc(s: usize) -> Option<usize> {
     unsafe {
         // Ensure BUS is initialized.
-        // SIZE is 1024 bits
-        pub const SIZE:usize = 0x80;
-        static mut BUS: [u8; SIZE] = [0u8; SIZE]; 
+        if BUS[0] != 0{
+            init();
+        }
 
-        // Reserve a block of s bytes
-        < 4 lines snipped > 
-
-        // Scan for a contigious region of size s
-        // In s > 8, word level allocation
-        // "Could be more efficient" it's an exercise!
-        < ~20 lines snipped> 
+        // find block, update bitmask
+        let address = search_space(s, res, 0);
+        match address {
+            Some(bindex) => { 
+                update_bmask(bindex, bindex+s);
+                Some(bindex)
+            }
+            None => None
+        }
     }
-    return None;
 }
+
+
+
+// Place val at loc
+// No safety checks so good luck out there.
+pub fn setter<T>(val: T, loc: usize) {
+    unsafe {
+        (&raw mut BUS).cast::<u8>().add(loc).cast::<T>().write(val);
+    }
+    return;
+}
+
+// Should check the validity bitmask search_spaceere...
+pub fn getter<T>(loc: usize) -> T {
+    unsafe {
+        (&raw mut BUS).cast::<u8>().add(loc).cast::<T>().read()
+    }
+}
+
+
+
+
+
+
+
