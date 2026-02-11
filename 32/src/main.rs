e3b4be0e558ac494d63c0701516ca02e1ce0c511
ggarver <gmgarver@willamette.edu>
2026-02-11 13:22:52 -0800

    Homework RISC-V

--- 32/src/main.rs
+++ 32/src/main.rs
@@ -1,12 +1,11 @@
-#![no_std]
 #![no_main]
-#[unsafe(no_mangle)]
+#![no_std]
 
-#[allow(unconditional_recursion)]
-
-pub extern "C" fn _start() -> ! { _start() }
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
 
 #[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    panic(info)
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
