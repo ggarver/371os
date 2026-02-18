495e8494e0339f8e88a4214c222a5fca852eaa2f
ggarver <gmgarver@willamette.edu>
2026-02-18 14:18:50 -0800

    format lab

---
+++ format/src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+mod vga;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    // use core::fmt::Write;
+    // let mut d = vga::Dummy { };
+    // let _ = write!(d, "Hello {}!", "world");
+    // vga::str_to_vga("Hello, world!");
+    loop {}
+}
+
+#[panic_handler]
+#[allow(unconditional_recursion)]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    println!("panic! at the panic handler");
+    loop {}
+    // panic(info)
+}
