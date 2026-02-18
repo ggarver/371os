9f285ea53d55e9b5342a2aa83637449da426b0e8
ggarver <gmgarver@willamette.edu>
2026-02-18 12:39:14 -0800

    lab + Hello_hw

--- 32/src/main.rs
+++ 32/src/main.rs
@@ -1,11 +1,17 @@
 #![no_main]
 #![no_std]
 
+use core::panic::PanicInfo;
+mod vga_buffer;
+
+#[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    println!("Hello World{}","!");
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
