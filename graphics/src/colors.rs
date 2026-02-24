af4f58a9be2870c5593b2310acbc5894d1782c51
ggarver <gmgarver@willamette.edu>
2026-02-24 15:47:51 -0800

    step 1 done

--- graphics/src/colors.rs
+++ graphics/src/colors.rs
@@ -1,14 +1,25 @@
 pub fn colors() {
+    // let colors: [u8; 16] = [0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf];
     // 720 x 400 pix
     // display background colors to screendump.ppm 
     // let color_codes: (*u8)= [];
     let vga_buff = 0xb8000 as *mut u8;
 
-    // 80 horizontal by 25 vert 
-    for i in 0..(80 * 25 * 2) {
-        unsafe {
-                // Write color byte
-                vga_buff.offset(i).write_volatile((i as u8) | 5);
+    // 80 horizontal by 25 vert, 2 dimensions
+    for row in 0..25_usize {
+        for col in 0..80_usize {
+
+            // 16 colors, this cycles through, int div
+            let color: u8 = (col / 5) as u8;
+            // slide over, * 2 to byte address
+            let offset = (row * 80 + col) * 2;
+
+            unsafe {
+                // 0xDB is solid block char 
+                vga_buff.add(offset).write_volatile(0xDB);
+                // color << 4 shift to background, 
+                vga_buff.add(offset + 1).write_volatile((color << 4) | color);
             }
         }
     }
+}
