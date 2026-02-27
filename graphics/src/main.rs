e08192792c09442c856887af8aee5d45be2e818b
ggarver <gmgarver@willamette.edu>
2026-02-26 20:17:48 -0800

    finished

--- graphics/src/main.rs
+++ graphics/src/main.rs
@@ -3,10 +3,11 @@
 
 mod colors;
 mod vga;
+mod image;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    colors::colors();
+    colors::image();
     // println!("Hello World{}", "!");
     // use core::fmt::Write;
     // let mut d = vga::Dummy { };
--- graphics/src/to_vga.py
+++ graphics/src/to_vga.py
@@ -7,16 +7,39 @@ def download(inp):
     response = requests.get(inp)
 
     as_file = io.BytesIO(response.content)
-    # img = Image.open(as_file)
+    img = Image.open(as_file)
+    scaled = img.resize((80, 25))
 
-    np_img = np.array(Image.open(img))
 
+    np_img = np.array(scaled)
+    cols = []
+
+    with Image.open("dump.ppm") as color_img:
+        for coordinate in range(0, 720, 45):
+            col = color_img.getpixel((coordinate, 0))
+            cols.append(col)
+    cols = np.array(cols)
+
+    # reshape to pixels instead of rows
+    pix = np_img.reshape(-1, 3)
+    # add dimensions in order to subtract 16 colors from 1 pix
+    # absolute val 
+    dist = np.abs(pix[:, np.newaxis, :] - cols[np.newaxis, :, :])
+    # sum to make distances easier
+    dist = dist.sum(axis = 2)
+
+    # find min dist, index = color 
+    indices = np.argmin(dist, axis=1)
+
+    data = ", ".join(map(str, indices))
+    with open("image.rs", "w") as w:
+        w.write(f"pub const IMG: [u8; {80 * 25}] = [{data}];")
 
 
 
 def main():
     # download(inp = input("enter img url:"))
-    download(inp = "https://cd-public.github.io/ai101/images/photo-cat.jpg")
+    download(inp = "https://cd-rs.github.io/os/img/rainbow.jpg")
 
 
 
