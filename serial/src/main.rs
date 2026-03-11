373b97ffcbc7a4fc1b1acc7d390c8598f0de0187
ggarver <gmgarver@willamette.edu>
2026-03-10 19:25:18 -0700

    tests partially working

--- serial/src/main.rs
+++ serial/src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 mod vga;
 mod serial;
@@ -7,13 +9,10 @@ mod serial;
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    use core::fmt::Write;
-    //let mut d = serial::Dummy { };
-    //let _ = write!(d, "Hello {}!", "world");
-    // use core::fmt::Write;
-    // let mut d = vga::Dummy { };
-    // let _ = write!(d, "Hello {}!", "world");
-    // vga::str_to_vga("Hello, world!");
+
+    #[cfg(test)]
+    test_runner(&[]);
+
     loop {}
 }
 
@@ -25,25 +24,27 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
     // panic(info)
 }
 
+fn t0(){
+    // test simple assertion 
+    println!("test_1");
+    print!("assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+fn t1(){
+    print!("test");
+}
+
 #[cfg(test)]
 fn test_runner(_tests: &[&dyn Fn()]) {
-    let fs = [_ex, _ex, _ex]; // _ex are supposed to be written tests 
+    let fs = [t0, t1]; // _ex are supposed to be written tests 
     for i in 0..fs.len() {
         serial_print!("Running test case {:0x}...", i);
         fs[i]();
-        serial_println!("Success!");
+        serial_println!(" [pass]");
     }
     unsafe { x86_64::instructions::port::Port::new(0xf4).write(0xAu32) }
 }
-
-fn test_serial_print(){
-    serial_print("print");
-}
-
-
-
-
-
-
 
 
