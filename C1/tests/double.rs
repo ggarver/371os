dc9d33c9c2da2d502e2c6525253b159ae6fd5f32
ggarver <gmgarver@willamette.edu>
2026-04-28 12:29:45 -0700

    all proper testing framework!

--- C1/tests/double.rs
+++ C1/tests/double.rs
@@ -1,29 +1,46 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(osirs::_test_runner)]
-#![feature(abi_x86_interrupt)]
+#![test_runner(test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
-use x86_64::structures::idt::InterruptStackFrame;
+use osirs::{serial_print, serial_println, qemu_quit, QemuExitCode};
 
 #[panic_handler]
-fn test_panic(info: &core::panic::PanicInfo) -> ! {
-    // osirs::serial_println!("{}", info);
-    osirs::serial_println!("[Pass]");
-    osirs::qemu_quit(osirs::QemuExitCode::Success);
+fn test_panic(_info: &core::panic::PanicInfo) -> ! {
+    serial_println!("[pass]");
+    qemu_quit(QemuExitCode::Success);
     loop {}
 }
 
-fn bad() {
-    unsafe { *(0xdeadbeef as *mut u8) = 42; };
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for (i, test) in tests.iter().enumerate() {
+        serial_print!("Running test {:0x}...", i);
+        test();
+        serial_println!("[fail] expected panic but none occurred");
+        qemu_quit(QemuExitCode::Failed);
+    }
+    qemu_quit(QemuExitCode::Failed);
 }
-
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     osirs::init();
-    osirs::_test_runner(&[&bad]);
-    osirs::qemu_quit(osirs::QemuExitCode::Failed);
+
+    #[cfg(test)]
+    test_main();
+
+    qemu_quit(QemuExitCode::Failed);
     loop {}
+}
+
+fn bad() {
+    unsafe { *(0xdeadbeef as *mut u8) = 42; }
+}
+
+#[test_case]
+fn test_bad() {
+    bad();
 }
--- C1/tests/except.rs
+++ C1/tests/except.rs
@@ -2,25 +2,27 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(osirs::_test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    osirs::_test_runner(&[&except_test]);
+    osirs::init();
+
+    #[cfg(test)]
+    test_main();
+
     osirs::qemu_quit(osirs::QemuExitCode::Failed);
     loop {}
 }
 
+#[test_case]
 fn except_test(){
     x86_64::instructions::interrupts::int3();
-    // breakpoint handler returns, execution resumes here
-    osirs::serial_println!("[ok] breakpoint exception returned");
 }
 
 #[panic_handler]
 fn test_panic(info: &core::panic::PanicInfo) -> ! {
-    osirs::serial_println!("[failed]");
-    osirs::qemu_quit(osirs::QemuExitCode::Failed);  // panic = failure
-    loop {}
+    osirs::test_panic_handler(info)
 }
