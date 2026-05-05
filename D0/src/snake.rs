35257d1434b7281e33170ee990aa854d5b7bf515
ggarver <gmgarver@willamette.edu>
2026-05-05 10:39:03 -0700

    got a food to show up :)

--- D0/src/snake.rs
+++ D0/src/snake.rs
@@ -11,54 +11,64 @@ use crate::println;
 
 pub fn border(){
     // draw first and last horizontal lines, 
-        for col in 1..79_usize {
-            let top_line = (0 * 80 + col) * 2;
-            let bot_line = (24 * 80 + col) * 2;
+    for col in 1..79_usize {
+        let top_line = (0 * 80 + col) * 2;
+        let bot_line = (24 * 80 + col) * 2;
 
-            unsafe {
-                // perhaps I should make this its own function...
-                BUFF_PTR.add(top_line).write_volatile(0xC4);
-                BUFF_PTR.add(top_line + 1).write_volatile(0x0f);
-
-
-                BUFF_PTR.add(bot_line).write_volatile(0xC4);
-                BUFF_PTR.add(bot_line + 1).write_volatile(0x0f);
-            }
-        }
-
-        // R and L lines 
-        for row in 1..24_usize {
-            let r_line = (row * 80 + 79) * 2;
-            let l_line = (row * 80 + 0) * 2;
-
-            unsafe {
-                BUFF_PTR.add(r_line).write_volatile(0xB3);
-                BUFF_PTR.add(r_line + 1).write_volatile(0x0f);
-
-                BUFF_PTR.add(l_line).write_volatile(0xB3);
-                BUFF_PTR.add(l_line + 1).write_volatile(0x0f);
-            }
-
-        }
-        // corners 
         unsafe {
-            let tl_c = (0 * 80 + 0) * 2;
-            BUFF_PTR.add(tl_c ).write_volatile(0xDA);
-            BUFF_PTR.add(tl_c + 1).write_volatile(0x0f);
-            let bl_c = (24 * 80 + 0) * 2;
-            BUFF_PTR.add(bl_c ).write_volatile(0xC0);
-            BUFF_PTR.add(bl_c + 1).write_volatile(0x0f);
-            let tr_c =(0 * 80 + 79) * 2;
-            BUFF_PTR.add(tr_c ).write_volatile(0xBF);
-            BUFF_PTR.add(tr_c + 1).write_volatile(0x0f);
-            let br_c =(24 * 80 + 79) * 2;
-            BUFF_PTR.add(br_c ).write_volatile(0xD9);
-            BUFF_PTR.add(br_c + 1).write_volatile(0x0f);
+            // perhaps I should make this its own function...
+            BUFF_PTR.add(top_line).write_volatile(0xC4);
+            BUFF_PTR.add(top_line + 1).write_volatile(0x0f);
 
+
+            BUFF_PTR.add(bot_line).write_volatile(0xC4);
+            BUFF_PTR.add(bot_line + 1).write_volatile(0x0f);
         }
     }
 
+    // R and L lines 
+    for row in 1..24_usize {
+        let r_line = (row * 80 + 79) * 2;
+        let l_line = (row * 80 + 0) * 2;
 
+        unsafe {
+            BUFF_PTR.add(r_line).write_volatile(0xB3);
+            BUFF_PTR.add(r_line + 1).write_volatile(0x0f);
+
+            BUFF_PTR.add(l_line).write_volatile(0xB3);
+            BUFF_PTR.add(l_line + 1).write_volatile(0x0f);
+        }
+
+    }
+    // corners 
+    unsafe {
+        let tl_c = (0 * 80 + 0) * 2;
+        BUFF_PTR.add(tl_c ).write_volatile(0xDA);
+        BUFF_PTR.add(tl_c + 1).write_volatile(0x0f);
+        let bl_c = (24 * 80 + 0) * 2;
+        BUFF_PTR.add(bl_c ).write_volatile(0xC0);
+        BUFF_PTR.add(bl_c + 1).write_volatile(0x0f);
+        let tr_c =(0 * 80 + 79) * 2;
+        BUFF_PTR.add(tr_c ).write_volatile(0xBF);
+        BUFF_PTR.add(tr_c + 1).write_volatile(0x0f);
+        let br_c =(24 * 80 + 79) * 2;
+        BUFF_PTR.add(br_c ).write_volatile(0xD9);
+        BUFF_PTR.add(br_c + 1).write_volatile(0x0f);
+
+    }
+}
+
+pub fn food(){
+        // I am so confused why nothing is showing up 
+        let foodspot = (10 * 80 + 10) * 2;
+
+        unsafe {
+            BUFF_PTR.add(foodspot).write_volatile(0x03);
+            BUFF_PTR.add(foodspot + 1).write_volatile(0x04);
+
+        }
+
+    }
 
 pub fn get_snake() -> &'static mut Snake {
     unsafe {
@@ -77,14 +87,16 @@ pub struct Snake {
 impl Snake {
     pub fn init_snake(&mut self) {
         border();
+        food();
         self.length = 1;
 
         // draw initial snake head at center (row 12, col 37)
         let pos = (12 * 80 + 37) * 2;
         unsafe {
-            BUFF_PTR.add(self.pos).write_volatile(BODY);
-            BUFF_PTR.add(self.pos).write_volatile(COL);
+            BUFF_PTR.add(pos).write_volatile(BODY);
+            BUFF_PTR.add(pos + 1).write_volatile(COL);
         }
+
 
         *get_snake() = Snake::new(self.length, self.pos);
     }
@@ -94,11 +106,10 @@ impl Snake {
     }
 
 
-
     pub fn right(&mut self){
         unsafe {
             if BUFF_PTR.add(self.pos + 2).read_volatile() == 0xB3 {
-            panic!();
+                panic!();
             }
         }
 
@@ -115,7 +126,7 @@ impl Snake {
     pub fn left(&mut self){
         unsafe {
             if BUFF_PTR.add(self.pos - 2).read_volatile() == 0xB3 {
-            panic!();
+                panic!();
             }
         }
 
@@ -132,7 +143,7 @@ impl Snake {
     pub fn up(&mut self){
         unsafe {
             if BUFF_PTR.add(self.pos - 160).read_volatile() == 0xC4 {
-            panic!();
+                panic!();
             }
         }
         self.pos = self.pos - 160;
@@ -148,7 +159,7 @@ impl Snake {
     pub fn down(&mut self){
         unsafe {
             if BUFF_PTR.add(self.pos + 160).read_volatile() == 0xC4 {
-            panic!();
+                panic!();
             }
         }
 
